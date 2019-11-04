a375050bb2c159400a878a4c8a18a40ee1fd48ba
donrol93 <donrol93@DESKTOP-6E02R5A.home>
2019-11-04 01:11:26 -0500

    go

--- CaseStudyMARolon/src/main/java/com/website/config/DataConfig.java
+++ CaseStudyMARolon/src/main/java/com/website/config/DataConfig.java
@@ -19,7 +19,7 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-//
+
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories("com.website.repository")
